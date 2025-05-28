7a865d004d91e48466836792dc37d7e046a1146c
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-05-28 01:27:38 +0100

    download works

--- s3-controller/src/viewkey.rs
+++ s3-controller/src/viewkey.rs
@@ -6,16 +6,31 @@ use axum::response::Response;
 use crate::appstate::AppState;
 
 pub(crate) async fn view_s3_key(
-    State(_state): State<AppState>,
+    State(state): State<AppState>,
     Path(key): Path<String>,
 ) -> Response {
     let start = Instant::now();
     println!("Fetching key: {}", key);
 
-    println!("Took: {} ms", start.elapsed().as_millis());
+    match state
+        .s3
+        .get_object()
+        .bucket(&state.bucket)
+        .key(&key)
+        .send()
+        .await
+    {
+        Ok(resp) => {
+            println!("Key fetched successfully: {}", key);
+            let body = resp.body.collect().await.unwrap().into_bytes();
 
-    return Response::builder()
-        .status(500)
-        .body("Not implemented".into())
-        .unwrap();
+            println!("Took: {} ms", start.elapsed().as_millis());
+
+            return Response::builder().status(200).body(body.into()).unwrap();
+        }
+        Err(e) => {
+            let msg = format!("Error fetching key: {}", e);
+            return Response::builder().status(500).body(msg.into()).unwrap();
+        }
+    }
 }
